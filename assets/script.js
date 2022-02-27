98abe8ec4ba2bf9316c6c604fe53b42032235f92
ancs214 <ancs214@gmail.com>
2022-02-27 13:35:03 -0500

    pseudocode for js file complete

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+
+//start quiz button click: timer starts and first question page presents itself
+
+//correct answer button click: next question page presents and notification of 'correct' pops up on new page at bottom with border styling
+//incorrect answer button click: next questions page presents and notification of 'incorrect' pops up on new page at bottom with border styling, time is also subtracted from clock
+//repeat this pattern for all questions
+
+//last question complete or timer reaches 0, end game: All done! page presents, final score posted, submit form for initials, and 'correct'/'incorrect pops up with border styling
+//initials and score saved to 'high scores'
+
+//on submit button click: highscores are posted, 'go back' button and 'clear highscores' button presents
+//'go back' button restarts quiz
+//'clear highscores' erases memory of highscores
+
+
+
+
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
